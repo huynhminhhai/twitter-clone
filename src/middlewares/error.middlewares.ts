d7afe4a8f7b3b223b24d35d8ac2643f366ea0aa9
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-05-01 13:26:00 +0700

    [UPDATE] defaultErrorHandler for throw new Error()

--- src/middlewares/error.middlewares.ts
+++ src/middlewares/error.middlewares.ts
@@ -3,5 +3,12 @@ import { omit } from 'lodash'
 import HTTP_STATUS from '~/constants/httpStatus'
 
 export const defaultErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  res.status(err.status || HTTP_STATUS.INTERNAL_SERVER_ERROR).json(omit(err, 'status'))
+  // res.status(err.status || HTTP_STATUS.INTERNAL_SERVER_ERROR).json(omit(err, 'status'))
+  res.status(err.status || HTTP_STATUS.INTERNAL_SERVER_ERROR).json(
+    err.status
+      ? omit(err, 'status')
+      : {
+          message: err.message
+        }
+  )
 }
